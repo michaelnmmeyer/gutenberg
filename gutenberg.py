4640b805a4c2f3c4c63ec791867b9b75c37e4a67
Michaël Meyer <hierophanie@gmail.com>
2016-03-03 11:04:48 +0100

    Add new encoding

--- gutenberg.py
+++ gutenberg.py
@@ -467,6 +467,7 @@ ENCS_TBL = {
    "ascii, with a few iso-8859-1 characters": None,
    "ascii, with a couple of iso-8859-1 characters": None,
    "ascii, with one iso-8859-1 character": None,
+   "ascii, with some iso-8859-1 characters": None,
    "windows codepage 1252": "Windows-1252",
    "cp-1252": "Windows-1252",
 }
